3d2f28ece218b05051da1b2707ce5a373f7656c8
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-08-15 23:34:39 +0300

    Updating 0-stats.py file

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,31 +8,30 @@ file_size = 0
 status_dict = {"200": 0, "301": 0, "400": 0, "401": 0,
                "403": 0, "404": 0, "405": 0, "500": 0}
 line_number = 0
-format_pattern = format_pattern = r'^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\s-\s\[' +\
-    r'[0-9]{2}/[A-Za-z]{3}/[0-9]{4}:[0-9]{2}:[0-9]{2}:[0-9]{2}\s' +\
-    r'\+\d{4}\]\s"\w+\s\/projects\/260\sHTTP\/1.1"\s[0-9]{3}\s[0-9]+$'
 
 try:
     for line in sys.stdin:
         line = line.strip()
         line_parts = line.split()
-        if re.match(format_pattern, line):
 
-            status_code = line_parts[7]
+        try:
+            status_code = line_parts[-2]
             if status_code in status_dict:
                 status_dict[status_code] += 1
-            try:
-                file_size += int(line_parts[8])
-            except Exception:
-                pass
-            line_number += 1
+        except Exception:
+            pass
+        try:
+            file_size += int(line_parts[-1])
+        except Exception:
+            pass
+        line_number += 1
 
-            if line_number == 10:
-                print("File size: {}".format(file_size))
-                for key, value in sorted(status_dict.items()):
-                    if value > 0:
-                        print("{}: {}".format(key, value))
-                line_number = 0
+        if line_number == 10:
+            print("File size: {}".format(file_size))
+            for key, value in sorted(status_dict.items()):
+                if value > 0:
+                    print("{}: {}".format(key, value))
+            line_number = 0
 
 except KeyboardInterrupt:
     pass
